2ae2ea096d7b7991bd4dc1f3d2abcbee30fce7c7
Ferney Tenorio <ferneybasto97@gmail.com>
2024-03-25 16:11:43 -0500

    update
    new desing

--- js/script.js
+++ js/script.js
@@ -1,98 +1,53 @@
-const pageTurnBtn = document.querySelectorAll('.nextprev-btn');
 
-pageTurnBtn.forEach((el, index) => {
-    el.onclick = () => {
-        const pageTurnId = el.getAttribute('data-page');
-        const pageTurn = document.getElementById(pageTurnId);
+/*===== MENU SHOW Y HIDDEN =====*/
+const navMenu = document.getElementById('nav-menu'),
+    toggleMenu = document.getElementById('nav-toggle'),
+    closeMenu = document.getElementById('nav-close');
 
-        if (pageTurn.classList.contains('turn')) {
-            pageTurn.classList.remove('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 - index;
-            }, 500);
+// SHOW
+toggleMenu.addEventListener('click', () => {
+    navMenu.classList.toggle('show')
+})
 
+// HIDDEN
+closeMenu.addEventListener('click', () => {
+    navMenu.classList.remove('show')
+})
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction() {
+    navMenu.classList.remove('show')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
         } else {
-            pageTurn.classList.add('turn');
-            setTimeout(() => {
-                pageTurn.style.zIndex = 20 + index;
-            }, 500);
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+
         }
-    }
-})
 
-//contact
-const pages = document.querySelectorAll('.book-page.page-right');
-const contactMe = document.querySelector('.btn.contact-me');
-
-contactMe.onclick = () => {
-    pages.forEach((page, index) => {
-        setTimeout(() => {
-            page.classList.add('turn');
-
-            setTimeout(() => {
-                page.style.zIndex = 20 + index;
-            }, 500);
-        }, (index + 1) * 200 + 100);
     })
 }
 
-//reverse index
-let totalPages = pages.length;
-let pageNumber = 0;
-
-function reverseIndex() {
-    pageNumber--;
-    if (pageNumber < 0) {
-        pageNumber = totalPages - 1;
-    }
-}
-
-//back profile
-const backProfileBtn = document.querySelector('.back-profile');
-
-backProfileBtn.onclick = () => {
-    pages.forEach((_, index) => {
-        setTimeout(() => {
-            reverseIndex();
-            pages[pageNumber].classList.remove('turn');
-
-            setTimeout(() => {
-                reverseIndex();
-                pages[pageNumber].style.zIndex = 10 + index;
-            }, 500);
-        }, (index + 1) * 200 + 100);
-    })
-}
-
-//cover right
-const coverRight = document.querySelector('.cover.cover-right');
-const pageLeft = document.querySelector('.book-page.page-left');
-setTimeout(() => {
-    coverRight.classList.add('turn')
-}, 2100);
-
-setTimeout(() => {
-    coverRight.style.zIndex=-1;
-}, 2800);
-
-//cover profile or left page
-setTimeout(() => {
-    pageLeft.style.zIndex=20;
-}, 3200);
-//first page
-pages.forEach((_, index) => {
-    setTimeout(() => {
-        reverseIndex();
-        pages[pageNumber].classList.remove('turn');
-
-        setTimeout(() => {
-            reverseIndex();
-            pages[pageNumber].style.zIndex = 10 + index;
-        }, 500);
-    }, (index + 1) * 200 + 2100);
-})
-
-//for contact
+//form contact
 const scriptURL = "https://script.google.com/macros/s/AKfycbzNslMW6Ag_vOr4goQ0LZELxlZ0_tD1g4td3RCsCf9rhc9XWJYYGt1aZJxDeRAUvNBT/exec";
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
@@ -136,3 +91,4 @@ function formatDate(date) {
     ].join(':')
   );
 }
+
